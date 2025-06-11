64487d7593051d447a234230f5781839a6500ead
pfremont <46523274+PaulFremont3@users.noreply.github.com>
2025-06-11 12:30:44 -0400

    Update run_simulations_SIVZ_and_SIVRZ_paremeters_optimisation_epipelagic_ocean.sh

--- Codes/Models/run_simulations_SIVZ_and_SIVRZ_paremeters_optimisation_epipelagic_ocean.sh
+++ Codes/Models/run_simulations_SIVZ_and_SIVRZ_paremeters_optimisation_epipelagic_ocean.sh
@@ -2,13 +2,8 @@
 
 for u in {0..3};
 do
-	echo $1
 	for c in {1..50};
 	do
-		echo $c
-		#sbatch run_SIVZ_and_SIVRZ_paremeters_optimisation_epipelagic_ocean.sbatch $u SIVZ $1 $c
 		sbatch run_SIVZ_and_SIVRZ_paremeters_optimisation_epipelagic_ocean.sbatch $u SIVZ_intra $1 $c
-		#sbatch run_SIVZ_and_SIVRZ_paremeters_optimisation_epipelagic_ocean.sbatch $u SIVRZ $1 $c
-		#sbatch run_SIVZ_and_SIVRZ_paremeters_optimisation_epipelagic_ocean.sbatch $u SIVRZ_intra $1 $c
 	done;
 done;
