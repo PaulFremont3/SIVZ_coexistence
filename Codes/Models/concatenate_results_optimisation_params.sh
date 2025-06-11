67239a32b98b56f43280eabe23568ae74ed99bfd
pfremont <46523274+PaulFremont3@users.noreply.github.com>
2025-06-11 12:30:01 -0400

    Add files via upload

---
+++ Codes/Models/concatenate_results_optimisation_params.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# remove file first
+if [[ ${4} == '1' ]]; then
+	rm "$1_$2_res_optimization_$3_POM.txt"
+elif [[ ${4} == '0' ]]; then
+	rm "$1_$2_res_optimization_$3.txt"
+fi
+
+for u in {1..50}; do
+    if [[ ${4} == '1' ]]; then
+        cat "results_optimization_params/$1_$2_res_optimization_$3_${u}_POM.txt" >> "$1_$2_res_optimization_$3_POM.txt"
+    elif [[ ${4} == '0' ]]; then
+        cat "results_optimization_params/$1_$2_res_optimization_$3_${u}.txt" >> "$1_$2_res_optimization_$3.txt"
+    fi	
+done
